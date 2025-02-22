407049e19f4542b77a9f13907f2f658796a1717e
Jose Ortiz <626055@bah.com>
2025-02-22 12:54:01 -0600

    uncomment next set of resources, remove google-beta provider

--- terraform/main.tf
+++ terraform/main.tf
@@ -62,9 +62,9 @@ resource "google_project_iam_member" "allbuild" {
   member     = "serviceAccount:${google_service_account.runsa.email}"
   depends_on = [google_project_service.all]
 }
-/*
+
 resource "google_compute_network" "main" {
-  provider                = google-beta
+  #provider                = google-beta
   name                    = "${var.basename}-private-network"
   auto_create_subnetworks = true
   project                 = var.project_id
@@ -73,7 +73,7 @@ resource "google_compute_network" "main" {
 
 resource "google_compute_global_address" "main" {
   name          = "${var.basename}-vpc-address"
-  provider      = google-beta
+  #provider      = google-beta
   purpose       = "VPC_PEERING"
   address_type  = "INTERNAL"
   prefix_length = 16
@@ -90,7 +90,7 @@ resource "google_service_networking_connection" "main" {
 }
 
 resource "google_vpc_access_connector" "main" {
-  provider       = google-beta
+  #provider       = google-beta
   project        = var.project_id
   name           = "${var.basename}-vpc-cx"
   ip_cidr_range  = "10.8.0.0/28"
@@ -99,7 +99,7 @@ resource "google_vpc_access_connector" "main" {
   max_throughput = 300
   depends_on     = [google_compute_global_address.main, google_project_service.all]
 }
-
+/*
 resource "random_id" "id" {
   byte_length = 2
 }
