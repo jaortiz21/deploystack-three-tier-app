e07a01a7478ec9cf7432cc143b0325e47c1e3d33
Jose Ortiz <626055@bah.com>
2025-02-22 15:41:32 -0600

    add debug, clean up wf rule

--- code/database/load_schema.sh
+++ code/database/load_schema.sh
@@ -1,6 +1,8 @@
 PROJECT=$1
 SQLNAME=$2
 
+gcloud info
+gcloud storage ls
 SQLSERVICEACCOUNT=$(gcloud sql instances describe $SQLNAME --project $PROJECT --format="value(serviceAccountEmailAddress)" | xargs)
 gsutil mb gs://$PROJECT-temp 
 gsutil cp schema.sql gs://$PROJECT-temp/schema.sql
