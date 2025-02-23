f4b10d27505388144f4782e33639bdb2743d704f
Jose Ortiz <626055@bah.com>
2025-02-23 13:10:16 -0600

    replace gsutil commands with gcloud

--- code/database/load_schema.sh
+++ code/database/load_schema.sh
@@ -3,11 +3,14 @@ SQLNAME=$2
 
 gcloud info
 SQLSERVICEACCOUNT=$(gcloud sql instances describe $SQLNAME --project $PROJECT --format="value(serviceAccountEmailAddress)" | xargs)
-gsutil mb gs://$PROJECT-temp 
-gsutil cp schema.sql gs://$PROJECT-temp/schema.sql
-gcloud storage buckets add-iam-policy-binding gs://$PROJECT-temp --member=allUsers --role=roles/storage.objectViewer
+gcloud storage buckets create gs://$PROJECT-temp --uniform-bucket-level-access
+#gsutil mb gs://$PROJECT-temp 
+gcloud storage cp schema.ql gs://$PROJECT-temp
+#gsutil cp schema.sql gs://$PROJECT-temp/schema.sql
+#gcloud storage buckets add-iam-policy-binding gs://$PROJECT-temp --member=allUsers --role=roles/storage.objectViewer
 gcloud storage ls gs://$PROJECT-temp
 gsutil iam ch serviceAccount:$SQLSERVICEACCOUNT:objectViewer gs://$PROJECT-temp/
 gcloud sql import sql $SQLNAME gs://$PROJECT-temp/schema.sql -q
-gsutil rm gs://$PROJECT-temp/schema.sql
-gsutil rb gs://$PROJECT-temp
+gcloud storage rm gs://$PROJECT-temp --recursive
+#gsutil rm gs://$PROJECT-temp/schema.sql
+#gsutil rb gs://$PROJECT-temp
