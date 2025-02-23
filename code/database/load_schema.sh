d458e4b206305e6c15a1a9ce7aecef79788f150a
Jose Ortiz <626055@bah.com>
2025-02-23 12:20:10 -0600

    add debug commands

--- code/database/load_schema.sh
+++ code/database/load_schema.sh
@@ -6,6 +6,8 @@ gcloud storage ls
 SQLSERVICEACCOUNT=$(gcloud sql instances describe $SQLNAME --project $PROJECT --format="value(serviceAccountEmailAddress)" | xargs)
 gsutil mb gs://$PROJECT-temp 
 gsutil cp schema.sql gs://$PROJECT-temp/schema.sql
+gcloud storage ls
+echo $SQLSERVICEACCOUNT
 gsutil iam ch serviceAccount:$SQLSERVICEACCOUNT:objectViewer gs://$PROJECT-temp/
 gcloud sql import sql $SQLNAME gs://$PROJECT-temp/schema.sql -q
 gsutil rm gs://$PROJECT-temp/schema.sql
