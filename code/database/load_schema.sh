d5f4886959c781c8d5cc4ca30c1751f7bb8bf991
Jose Ortiz <626055@bah.com>
2025-02-23 12:43:29 -0600

    add policy binding for bucket access

--- code/database/load_schema.sh
+++ code/database/load_schema.sh
@@ -2,12 +2,11 @@ PROJECT=$1
 SQLNAME=$2
 
 gcloud info
-gcloud storage ls
 SQLSERVICEACCOUNT=$(gcloud sql instances describe $SQLNAME --project $PROJECT --format="value(serviceAccountEmailAddress)" | xargs)
 gsutil mb gs://$PROJECT-temp 
 gsutil cp schema.sql gs://$PROJECT-temp/schema.sql
-gcloud storage ls
-echo $SQLSERVICEACCOUNT
+gcloud storage buckets add-iam-policy-binding gs://$PROJECT-temp --member=allUsers --role=roles/storage.objectViewer
+gcloud storage ls gs://$PROJECT-temp
 gsutil iam ch serviceAccount:$SQLSERVICEACCOUNT:objectViewer gs://$PROJECT-temp/
 gcloud sql import sql $SQLNAME gs://$PROJECT-temp/schema.sql -q
 gsutil rm gs://$PROJECT-temp/schema.sql
