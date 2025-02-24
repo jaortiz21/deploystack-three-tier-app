a5b11caab945d8ee886720a44dbebac70ba1fcfa
Jose Ortiz <626055@bah.com>
2025-02-23 19:19:13 -0600

    update schema

--- code/database/load_schema.sh
+++ code/database/load_schema.sh
@@ -7,9 +7,9 @@ gcloud storage buckets create gs://$PROJECT-temp --uniform-bucket-level-access
 #gsutil mb gs://$PROJECT-temp 
 gcloud storage cp schema.sql gs://$PROJECT-temp
 #gsutil cp schema.sql gs://$PROJECT-temp/schema.sql
-#gcloud storage buckets add-iam-policy-binding gs://$PROJECT-temp --member=allUsers --role=roles/storage.objectViewer
+gcloud storage buckets add-iam-policy-binding gs://$PROJECT-temp --member=allUsers --role=roles/storage.objectViewer
 gcloud storage ls gs://$PROJECT-temp
-gsutil iam ch serviceAccount:$SQLSERVICEACCOUNT:objectViewer gs://$PROJECT-temp/
+#gsutil iam ch serviceAccount:$SQLSERVICEACCOUNT:objectViewer gs://$PROJECT-temp/
 gcloud sql import sql $SQLNAME gs://$PROJECT-temp/schema.sql -q
 gcloud storage rm gs://$PROJECT-temp --recursive
 #gsutil rm gs://$PROJECT-temp/schema.sql
